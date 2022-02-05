c15cc57e333fbfdc9bec97e34cb0a5b33c244f23
AnmolpreetKaur10861 <kau10861@sheridancollege.ca>
2022-02-04 22:39:46 -0500

    1st commit

---
+++ src/softwaredesign/SoftwareDesign.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package softwaredesign;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author anmol
+ */
+public class SoftwareDesign {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        //CHANGE THE ARRAY SIZE
+        Student[] students = new Student[4];
+        
+        for (int i = 0; i < students.length; i++) {
+            System.out.println("Enter the name for student " + (i + 1));
+            String name = input.nextLine();
+            Student student = new Student(name);
+            students[i] = student;
+        }
+
+        String format = "Student's name is %s\n";
+        
+        for (Student student: students) {
+            System.out.printf(format, student.getName());            
+        }        
+    }
+    
+}
